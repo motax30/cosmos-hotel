734189082fc006473d9e46c9f9dae2815b10df1b
motax30 <motax30@gmail.com>
2017-08-15 19:18:34 -0300

    Adiciona um projeto maven com as configurações iniciais.

---
+++ cosmos/src/main/java/SIGH/br/com/sjc/fatec/App.java
@@ -0,0 +1,13 @@
+package SIGH.br.com.sjc.fatec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
